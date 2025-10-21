c937962a7e600ca19c2dbd73eb7e01f0f9ab34b1
Hantan <tease@eteb.me>
2025-10-21 15:33:51 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/cryptooptimizer/main.go
@@ -0,0 +1,23 @@
+// cmd/cryptooptimizer/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "cryptooptimizer/internal/cryptooptimizer"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := cryptooptimizer.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
